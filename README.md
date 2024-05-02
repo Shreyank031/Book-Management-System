e5c588269ef94c6f336ca4e8af6bd7df775c6af0
Shreyank031 <shreyankchandu@gmail.com>
2024-05-02 22:01:58 +0530

    Add PATCH request to check out as well as return the book

---
+++ README.md
@@ -0,0 +1,47 @@
+# Book Management System
+
+This is a simple RESTful API for managing a collection of books.
+
+## Gin Framework
+
+Gin is a web framework written in Go (or Golang) which is known for its minimalistic design and high performance. It's commonly used for building web applications and RESTful APIs due to its fast routing and processing capabilities.
+
+Usage
+-----
+
+Once the application is running, you can interact with it using HTTP requests. Here are the available endpoints:
+
+- `GET /books`: Retrieve all books.
+- `POST /books`: Add a new book to the collection.
+- `GET /books/:id`: Retrieve details of a specific book by its ID.
+- `PATCH /checkout?id=:id`: Check out a book by its ID.
+- `PATCH /return?id=:id`: Return a book by its ID.
+
+- **Retrieve all books**: 
+  `GET /books`  
+
+```bash
+  curl localhost:8080/books
+  ```
+
+- **Add a new book** :
+  `POST /books`
+  ```bash
+  curl localhost:8080/books --include --header "Content-Type: applicationo/json" -d @<file_name_json> --request "POST"
+  ```
+- **Retrieve details of a specific book**:
+  `GET /books/:id`
+  ```bash
+  curl localhost:8080/books/1 
+  ```
+- **Check out a book**:
+  `PATCH /checkout?id=:id`
+  ```bash
+  curl 'localhost:8080/checkout?id=1' --request "PATCH"
+  ```
+- **Return a book**:
+  `PATCH /return?id=:id`
+  ```bash
+  curl 'localhost:8080/return?id=1' --request "PATCH"
+  ```
+
--- main.go
+++ main.go
@@ -7,8 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//To store our book we are going to use a struct
-
+// Define a struct called "book" to store information about each book
 type book struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
@@ -16,16 +15,19 @@ type book struct {
 	Quantity int    `json:"quantity	"`
 }
 
+// getBooks returns the list of all books
 var books = []book{
 	{ID: "1", Title: "In Search Of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
 
+// getBooks returns the list of all books
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+// createBooks adds a new book to the list
 func createBooks(c *gin.Context) {
 	var newBook book
 	err := c.BindJSON(&newBook)
@@ -36,6 +38,7 @@ func createBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
+// bookById retrieves a book by its ID
 func bookById(c *gin.Context) {
 	id := c.Param("id")
 	book, err := getBookId(id)
@@ -46,6 +49,7 @@ func bookById(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, book)
 }
 
+// getBookById returns a book by its ID
 func getBookId(id string) (*book, error) {
 	for i, b := range books {
 		if b.ID == id {
@@ -55,10 +59,59 @@ func getBookId(id string) (*book, error) {
 	return nil, errors.New("book not found!")
 }
 
+// checkOutBook allows a user to check out a book
+func checkOutBook(c *gin.Context) {
+
+	id, ok := c.GetQuery("id")
+	if !ok {
+		c.IndentedJSON(http.StatusBadGateway, gin.H{"message": "Missing id query parameter"})
+		return
+	}
+
+	book, err := getBookId(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available"})
+		return
+	}
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+// returnBook allows a user to return a book
+func returnBook(c *gin.Context) {
+
+	id, ok := c.GetQuery("id")
+	if !ok {
+		c.IndentedJSON(http.StatusBadGateway, gin.H{"message": "Missing id query parameter"})
+		return
+	}
+
+	book, err := getBookId(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available"})
+		return
+	}
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/books", getBooks)
-	router.POST("/books", createBooks) //we'll be using the same endpoint
+	router.GET("/books", getBooks)     //get req-> getting information
+	router.POST("/books", createBooks) //post -> adding/creating new information
 	router.GET("/books/:id", bookById)
-	router.Run("localhost:8080")
+	router.PATCH("/checkout", checkOutBook) //patch -> updating
+	router.PATCH("/return", returnBook)
+	router.Run("localhost:8080") // Start the server on localhost:8080
 }
